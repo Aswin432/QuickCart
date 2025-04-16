d58f24fe0a9ddaecc3e7592da26212edd9364759
aswin718 <aswinachur7181@gmail.com>
2025-04-17 01:29:25 +0530

    commmit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,12 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/config/inngest";
-import { helloWorld } from "../../../inngest/functions";
+import {
+  inngest,
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+} from "@/config/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    helloWorld, // <-- This is where you'll always add all your functions
-  ],
+  functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
 });
