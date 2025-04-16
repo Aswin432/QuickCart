0b811ac0ac2c84c8fce8d10e80a27e55486f4a54
aswin718 <aswinachur7181@gmail.com>
2025-04-17 01:52:19 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function connectDb() {
+async function connectDB() {
   if (cached.conn) {
     return cached.conn;
   }
